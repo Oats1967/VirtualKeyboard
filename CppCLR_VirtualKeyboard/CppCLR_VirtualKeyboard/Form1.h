0424748137d227fb561b0c53239d60d5965443b2
Luca Hafer <luca.hafer01@gmail.com>
2023-12-26 17:25:11 +0100

    first UI elements

--- CppCLR_VirtualKeyboard/CppCLR_VirtualKeyboard/Form1.h
+++ CppCLR_VirtualKeyboard/CppCLR_VirtualKeyboard/Form1.h
@@ -34,6 +34,13 @@ namespace CppCLRWinFormsProject {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::CheckBox^ chkCapsLock;
+	protected:
+
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::CheckBox^ checkBox1;
+	protected:
+
 
 	private:
 		/// <summary>
@@ -48,13 +55,61 @@ namespace CppCLRWinFormsProject {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->chkCapsLock = (gcnew System::Windows::Forms::CheckBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
 			this->SuspendLayout();
+			// 
+			// chkCapsLock
+			// 
+			this->chkCapsLock->Appearance = System::Windows::Forms::Appearance::Button;
+			this->chkCapsLock->BackColor = System::Drawing::Color::Black;
+			this->chkCapsLock->Font = (gcnew System::Drawing::Font(L"Times New Roman", 7.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->chkCapsLock->ForeColor = System::Drawing::Color::White;
+			this->chkCapsLock->Location = System::Drawing::Point(2, 177);
+			this->chkCapsLock->Name = L"chkCapsLock";
+			this->chkCapsLock->Size = System::Drawing::Size(52, 52);
+			this->chkCapsLock->TabIndex = 0;
+			this->chkCapsLock->Text = L"Caps Lock";
+			this->chkCapsLock->UseVisualStyleBackColor = false;
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::Color::Black;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 7.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->ForeColor = System::Drawing::Color::White;
+			this->button1->Location = System::Drawing::Point(256, 126);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(52, 52);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
+			// 
+			// checkBox1
+			// 
+			this->checkBox1->Appearance = System::Windows::Forms::Appearance::Button;
+			this->checkBox1->BackColor = System::Drawing::Color::Black;
+			this->checkBox1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 7.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->checkBox1->ForeColor = System::Drawing::Color::White;
+			this->checkBox1->Location = System::Drawing::Point(2, 226);
+			this->checkBox1->Name = L"checkBox1";
+			this->checkBox1->Size = System::Drawing::Size(52, 52);
+			this->checkBox1->TabIndex = 2;
+			this->checkBox1->Text = L"Shift";
+			this->checkBox1->UseVisualStyleBackColor = false;
 			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(993, 324);
+			this->Controls->Add(this->checkBox1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->chkCapsLock);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
 			this->Name = L"Form1";
 			this->ShowInTaskbar = false;
@@ -64,5 +119,9 @@ namespace CppCLRWinFormsProject {
 
 		}
 #pragma endregion
+	private: System::Void checkBox2_CheckedChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
 	};
 }
